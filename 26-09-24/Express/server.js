0180fa8183613f1896f3000947bd0c4cb0b283e7
vikramgit123 <22641a0527@vaagdevi.edu.in>
2024-09-27 10:22:26 +0530

    updated untill 2sep24

--- 26-09-24/Express/server.js
+++ 26-09-24/Express/server.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyparser =require("body-parser");
+app.use(bodyparser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//app.get('/', (req, res) => {
+ // res.send('Hello World!')
+ // res.sendStatus(404)
+ // res.download("1.webp")
+//})
+
+app.post("/postuserinfo", (req,res) =>{
+  const id=req.body.name;
+  //const email=req.body,email;
+  const phone=req.body.phone;
+  res.send('data received sucessfully')
+ // res.send(JSON.stringify(req.body);)
+
+
+});
+
+app.get("/product")
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
